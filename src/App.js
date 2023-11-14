6800c8e7d571c9826a4836fa1e529ed7c26e055a
aldinhalilovic <aldinhalilovic06@gmail.com>
2023-11-14 17:02:08 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header";
+import HomePage from "./page/HomePage";
+import About from "./page/About";
+import Contact from "./page/Contact";
+import Cart from "./page/Cart";
+import Profile from "./page/Profile";
+import CartContextProvider from "./store/CartContext";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <div style={{ padding: "0 20%" }}>
+          <CartContextProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </CartContextProvider>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
